3bd3ea130884bf39f43d8f0bc2253be42a56488a
MalliAutomation <dgmarjun@xlbyte.com>
2022-02-09 11:38:30 +0530

    Deleting some code

--- src/main/java/concatintae.java
+++ src/main/java/concatintae.java
@@ -6,8 +6,6 @@ public class concatintae {
 		String str1="ABC";
 		String str2="XYZ";
 		String str3="PQR";
-		String str4=str1+str2+str3;
-		System.out.println(str4);
 		System.out.println(str1.concat(str2).concat(str3));
 		
 
